f429ce373252d0e96f18d329c5e9f170b853fa4d
Alejandro Leiva <65797986+aleivaar94@users.noreply.github.com>
2023-01-20 13:23:38 -0800

    updated relative path

--- noc-dash-streamlit.py
+++ noc-dash-streamlit.py
@@ -3,8 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import json
 from PIL import Image
-import os
-
+from pathlib import Path
 
 st.markdown("<h1 style='text-align: center'>NOC Wage Finder</h1>", unsafe_allow_html=True)
 
@@ -19,7 +18,6 @@ merged['noc_2016'] = merged['noc_2016'].str.zfill(4)
 merged['noc_2021'] = merged['noc_2021'].astype('Int64')
 merged['noc_2021'] = merged['noc_2021'].astype(str)
 
-path = os.getcwd()
 
 with st.sidebar:
 
@@ -29,15 +27,15 @@ with st.sidebar:
     
     st.header('How to find my NOC Code and/or NOC Title?')
     st.markdown('1. 💻 Go to [National Occupation Classification](https://noc.esdc.gc.ca/) Select the NOC Version and input your job title:')
-    image = Image.open(path + "\\assets\\select-noc.png")
+    image = Image.open(Path(__file__).parents[1] / 'assets/select-noc.png')
     st.image(image)
 
     st.markdown('2. 👆 Select the unit group that matches your job title.')
-    image = Image.open(path + "\\assets\\select-unit-group.png")
+    image = Image.open(Path(__file__).parents[1] / 'assets/select-unit-group.png')
     st.image(image)
 
     st.markdown('3. ✅ If there is more than one unit group, select the unit group that mostly resembles. Click on it and match *Example titles* and *Main duties* with your current job title.')
-    image = Image.open(path + "\\assets\\verify-unit-group.png")
+    image = Image.open(Path(__file__).parents[1] / 'assets/verify-unit-group.png')
     st.image(image)
 
 # Select NOC version
